3ef73d01e4f54c68672fc6cf9613f3281e08e0b8
erhnaks <erhnaks@hotmail.com>
2022-06-29 16:57:17 +0100

    Logging has been debugged and it is now working

---
+++ com.qa.junitTesting/src/com/qa/logging/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.qa.logging;
+
+//import org.apache.logging.log4j.core.*;
+import java.io.*;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+public class HelloWorld {
+
+	static Logger log = Logger.getLogger(HelloWorld.class.getName());
+
+	public static void main(String[] args) throws IOException, SQLException {
+
+		// log.debug("Hello this is an debug message");
+		log.info("Hello this is an info message");
+		log.warning("This is your last warning...");
+	}
+}
